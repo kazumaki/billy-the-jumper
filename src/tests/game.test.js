11dce0cc62d37e9995e13368f80945712c4b883e
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-04-20 21:31:40 -0300

    Add jest tests and some tests to game module

--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -5,3 +5,15 @@ let game;
 beforeAll(() => {
   game = new Game();
 });
+
+test('Expect score to be 0 when start the game', () => {
+  expect(game.globals.score).toBe(0);
+});
+
+test('Expect sound to be enable when the game start', () => {
+  expect(game.globals.model.soundOn).toBe(true);
+});
+
+test('Expect music to be enable when the game start', () => {
+  expect(game.globals.model.musicOn).toBe(true);
+});
